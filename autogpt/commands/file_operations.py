8196d5b8d45af519f46f1f6f4b7748e6ed45f779
cryptonaut <nrathman@ironcladtech.ca>
2023-05-04 21:08:44 -0700

    expand the types of file operations possible

--- autogpt/commands/file_operations.py
+++ autogpt/commands/file_operations.py
@@ -3,8 +3,12 @@ from __future__ import annotations
 
 import hashlib
 import os
+import stat
 import os.path
 from typing import Dict, Generator, Literal, Tuple
+import shutil
+import tarfile
+import zipfile
 
 import charset_normalizer
 import requests
@@ -341,3 +345,278 @@ def download_file(url, filename):
         return f"Got an HTTP Error whilst trying to download file: {err}"
     except Exception as err:
         return f"Error: {err}"
+
+
+#BEGIN CUSTOM METHODS
+
+@command("create_directory", "Creates a new File System Directory", '"directory_path_name": "<directory_path_name>"')
+def create_directory(directory_path_name: str) -> str:
+    '''Creates a directory with the given name.'''
+    try:
+        os.makedirs(directory_path_name)
+        return f"Directory '{directory_path_name}' created."
+    except FileExistsError:
+        return f"Directory '{directory_path_name}' already exists."
+    except Exception as e:
+        return f"Error creating directory '{directory_path_name}': {e}"
+
+@command("rename_directory", "Rename a Directory", '"old_path": "<old_path>", "new_path": "<new_path>"')
+def rename_directory(old_path: str, new_path: str) -> str:
+    '''Renames a directory from old_name to new_name.'''
+    try:
+        os.rename(old_name, new_name)
+        return f"Directory renamed from '{old_name}' to '{new_name}'."
+    except FileNotFoundError:
+        return f"Directory '{old_name}' not found."
+    except Exception as e:
+        return f"Error renaming directory '{old_name}' to '{new_name}': {e}"
+
+@command("delete_directory", "Delete a Directory and its contents", '"directory_path": "<directory_path>"')
+def delete_directory(directory_path: str) -> str:
+    '''Deletes a directory with the given path.'''
+    try:
+        shutil.rmtree(directory_path)
+        return f"Directory '{directory_path}' deleted."
+    except FileNotFoundError:
+        return f"Directory '{directory_path}' not found."
+    except Exception as e:
+        return f"Error deleting directory '{directory_path}': {e}"
+
+@command("copy_file", "Make a duplicate copy of a file", '"source": "<source>", "destination": "<destination>"')
+def copy_file(source: str, destination: str) -> str:
+    '''Copies a file from the source path to the destination path.'''
+    try:
+        shutil.copy2(source, destination)
+        return f"File '{source}' copied to '{destination}'."
+    except FileNotFoundError:
+        return f"File '{source}' not found."
+    except Exception as e:
+        return f"Error copying file '{source}' to '{destination}': {e}"
+
+@command("rename_file", "Rename a file", '"old_path": "<old_path>", "new_path": "<new_path>"')
+def rename_file(old_path: str, new_path: str) -> str:
+    '''Renames a file from old_path to new_path.'''
+    try:
+        os.rename(old_path, new_path)
+        return f"File renamed from '{old_name}' to '{new_path}'."
+    except FileNotFoundError:
+        return f"File '{old_path}' not found."
+    except Exception as e:
+        return f"Error renaming file '{old_path}' to '{new_path}': {e}"
+
+@command("get_file_info", "Get info for a file including mime type, permissions and more", '"file_path": "<file_path>"')
+def get_file_info(file_path: str) -> str:
+    '''Returns a formatted string with file info (including mime type, permissions, and ownership) and metadata for the given file path.'''
+    try:
+        stat = os.stat(file_path)
+
+        # Get mime type
+        mime_type, encoding = mimetypes.guess_type(file_path)
+
+        # Get permissions
+        permissions = oct(stat.st_mode)[-3:]
+
+        # Get ownership
+        owner = pwd.getpwuid(stat.st_uid).pw_name
+        group = grp.getgrgid(stat.st_gid).gr_name
+
+        # Format the file information as a string
+        file_info = f"""File info for '{file_path}':
+- Size: {stat.st_size} bytes
+- Creation time: {datetime.datetime.fromtimestamp(stat.st_ctime).strftime('%Y-%m-%d %H:%M:%S')}
+- Last modified time: {datetime.datetime.fromtimestamp(stat.st_mtime).strftime('%Y-%m-%d %H:%M:%S')}
+- Last access time: {datetime.datetime.fromtimestamp(stat.st_atime).strftime('%Y-%m-%d %H:%M:%S')}
+- Mode: {stat.st_mode}
+- UID: {stat.st_uid}
+- GID: {stat.st_gid}
+- Mime type: {mime_type}
+- Permissions: {permissions}
+- Owner: {owner}
+- Group: {group}
+"""
+
+        return file_info
+    except FileNotFoundError:
+        return f"File '{file_path}' not found."
+    except Exception as e:
+        return f"Error getting file info for '{file_path}': {e}"
+
+@command(
+    "change_permissions",
+    "Change permissions of a target file or directory",
+    '"target_path": "<target_path>", "permissions": "<permissions>"',
+    None,
+    None,
+)
+def change_permissions(target_path: str, permissions: str) -> str:
+    """Change permissions of a target file or directory recursively.
+    Args:
+        target_path (str): The path to the target file or directory.
+        permissions (str): The new permissions in octal format (e.g., '755').
+    Returns:
+        str: The result of the chmod operation.
+    """
+    try:
+        # Convert the permissions string to an integer
+        octal_permissions = int(permissions, 8)
+
+        if os.path.isfile(target_path):
+            # Change the permissions of the target file
+            os.chmod(target_path, octal_permissions)
+        elif os.path.isdir(target_path):
+            # Change the permissions of the target directory and its contents
+            for root, dirs, files in os.walk(target_path):
+                os.chmod(root, octal_permissions)
+                for file in files:
+                    os.chmod(os.path.join(root, file), octal_permissions)
+        else:
+            return f"Error: '{target_path}' is not a file or directory."
+
+        return f"Changed permissions of '{target_path}' to {permissions} recursively."
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+@command(
+    "change_owner_group",
+    "Change owner and group (chown) of a target file or directory",
+    '"target_path": "<target_path>", "owner": "<owner>", "group": "<group>"'
+)
+def change_owner_group(target_path: str, owner: str, group: str) -> str:
+    """Change owner and group of a target file or directory.
+    Args:
+        target_path (str): The path to the target file or directory.
+        owner (str): The new owner's username.
+        group (str): The new group's name.
+    Returns:
+        str: The result of the chown operation.
+    """
+    try:
+        # Get the user ID and group ID
+        uid = os.getpwnam(owner).pw_uid
+        gid = os.getgrnam(group).gr_gid
+
+        # Change the owner and group of the target file or directory
+        os.chown(target_path, uid, gid)
+
+        return f"Changed owner and group of '{target_path}' to {owner}:{group}."
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+@command(
+    "get_file_size",
+    "Get the file size in bytes for a chosen file path or directory",
+    '"target_path": "<target_path>"'
+)
+def get_file_size(target_path: str) -> str:
+    """Get the file size in bytes for a chosen file path or directory
+    Args:
+        target_path (str): The path to the target file or directory.
+    Returns:
+        str: The result containing the file size or the total combined bytes of the directory.
+    """
+    try:
+        if os.path.isfile(target_path):
+            # Get the size of the target file
+            size = os.path.getsize(target_path)
+            return f"File size of '{target_path}' is {size} bytes."
+        elif os.path.isdir(target_path):
+            # Calculate the total combined bytes and number of files in the directory
+            total_size = 0
+            total_files = 0
+            for root, dirs, files in os.walk(target_path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    total_size += os.path.getsize(file_path)
+                    total_files += 1
+            return f"Total size of '{target_path}' is {total_size} bytes with {total_files} files."
+        else:
+            return f"Error: '{target_path}' is not a file or directory."
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+@command(
+    "compress_files",
+    "Compress files at target paths into an archive with a chosen format",
+    '"target_paths": "<target_paths>", "compression_format": "<compression_format>", "output_file": "<output_file>"'
+)
+def compress_files(target_paths: str, compression_format: str = "zip", output_file: str = "output") -> str:
+    """Compress files at target paths into an archive with a chosen format.
+    Args:
+        target_paths (str): Comma-separated paths of the target files or directories.
+        compression_format (str, optional): The chosen compression format. Defaults to 'zip'.
+        output_file (str, optional): The name of the output file without extension. Defaults to 'output'.
+    Returns:
+        str: The result of the compress operation.
+    """
+    try:
+        paths = [path.strip() for path in target_paths.split(',')]
+        archive_path = f"{output_file}.{compression_format}"
+
+        if compression_format == "zip":
+            with shutil.make_archive(output_file, 'zip', base_dir='.') as archive:
+                for path in paths:
+                    if os.path.exists(path):
+                        archive.write(path, os.path.basename(path))
+        elif compression_format in ["tar", "gz", "bz2"]:
+            mode = "w"
+            if compression_format == "gz":
+                mode = "w:gz"
+            elif compression_format == "bz2":
+                mode = "w:bz2"
+
+            with tarfile.open(archive_path, mode) as archive:
+                for path in paths:
+                    if os.path.exists(path):
+                        archive.add(path, arcname=os.path.basename(path))
+        else:
+            return f"Error: Unsupported compression format '{compression_format}'."
+
+        return f"Compressed files into '{archive_path}'."
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+@command(
+    "uncompress_archive",
+    "Uncompress a .zip, .tar, .gz or .bz2 archive file to a directory",
+    '"archive_path": "<archive_path>"'
+)
+def uncompress_archive(archive_path: str) -> str:
+    """Uncompress an archive file based on its extension or mime type.
+    Args:
+        archive_path (str): The path to the archive file.
+    Returns:
+        str: The result of the uncompress operation.
+    """
+    try:
+        if not os.path.isfile(archive_path):
+            return f"Error: '{archive_path}' is not a file."
+
+        file_name, file_ext = os.path.splitext(archive_path)
+        output_dir = file_name
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        if file_ext.lower() == ".zip":
+            with zipfile.ZipFile(archive_path, 'r') as archive:
+                archive.extractall(output_dir)
+        elif file_ext.lower() in [".tar", ".gz", ".bz2"]:
+            mode = "r"
+            if file_ext.lower() == ".gz":
+                mode = "r:gz"
+            elif file_ext.lower() == ".bz2":
+                mode = "r:bz2"
+
+            with tarfile.open(archive_path, mode) as archive:
+                archive.extractall(output_dir)
+        else:
+            return f"Error: Unsupported archive format '{file_ext}'."
+
+        return f"Uncompressed '{archive_path}' into '{output_dir}'."
+
+    except Exception as e:
+        return f"Error: {str(e)}"
